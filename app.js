052ba49301467c9486f6b07075d36bb51701a9b9
Nicolaj2903 <Iversen2903@hotmail.com>
2024-07-23 09:19:09 +0200

    Navigation using ctrl + arrow keys now work as intended

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
+
 // Return
 const returnValue = document.getElementById("value");
-
-// Exchange rate
-const DKK = 0;
-const USD = 6.86;
-const EURO = 7.46;
-
-// Colors
-const red = "red";
-const green = "green";
-const grey = "#222";
 
 // Buttons
 const btns = document.querySelectorAll(".btn");
@@ -19,38 +10,27 @@ const toggleModeButton = document.querySelector(".mode-toggle");
 const currentPriceInput = document.getElementById("currentPrice");
 const salesPriceInput = document.getElementById("salesPrice");
 const shareAmountInput = document.getElementById("shareAmount");
-const exchangeFeeInput = document.getElementById("vekselgebyr");
 const inputs = document.querySelectorAll(".form-input");
-
-// Dropdown
-const currencyDropdown = document.querySelector(".dropdown-currency");
 
 // General text
 const missingInputWarning = "Udfyld felter";
 const invalidInputWarning = "Ugyldig input";
 
-// Object-literal
-const currencies = {
-    "dkk": capitalGainDK,
-    "usd": capitalGainUSD,
-    "euro": capitalGainEURO
-};
+// Colors
+const red = "red";
+const green = "green";
+const grey = "#222";
+
 
 // Event listeners
-currencyDropdown.addEventListener("change", function () {
-    toggleExchangeFee();
-});
-
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const styles = e.currentTarget.classList;
 
         if (styles.contains("afkast")) {
+
             if (validateInputsAfkastBtn()) {
-                const selectedCurrency = currencyDropdown.value.toLowerCase();
-                if (currencies[selectedCurrency]) {
-                    currencies[selectedCurrency]();
-                }
+                capitalGain();
             }
         } else if (styles.contains("procent")) {
             if (validateInputsProcentBtn()) {
@@ -64,13 +44,14 @@ btns.forEach(function (btn) {
 
 inputs.forEach((input, index) => {
     input.addEventListener("keydown", (event) => {
-        if ((event.ctrlKey && event.key === "ArrowUp") || event.key === "ArrowLeft") {
+        if (event.ctrlKey && event.key === "ArrowUp" || event.key === "ArrowLeft") {
             event.preventDefault();
+
             const previousIndex = (index - 1 + inputs.length) % inputs.length;
             inputs[previousIndex].focus();
         }
 
-        if ((event.ctrlKey && event.key === 'ArrowDown') || event.key === "ArrowRight") {
+        if (event.ctrlKey && event.key === 'ArrowDown' || event.key === "ArrowRight") {
             event.preventDefault();
             const nextIndex = (index + 1) % inputs.length;
             inputs[nextIndex].focus();
@@ -78,22 +59,43 @@ inputs.forEach((input, index) => {
     });
 });
 
-// Handle Enter key press
-function handleEnterKey(event, action) {
+// event.keyCode === 13 <=> Pressing Enter
+currentPriceInput.addEventListener("keydown", function (event) {
     if (event.ctrlKey && event.keyCode === 13) {
         event.preventDefault();
         reset();
     } else if (event.keyCode === 13) {
         event.preventDefault();
         if (validateInputsAfkastBtn()) {
-            action();
+            capitalGain();
         }
     }
-}
+});
 
-currentPriceInput.addEventListener("keydown", (event) => handleEnterKey(event, capitalGainDK));
-salesPriceInput.addEventListener("keydown", (event) => handleEnterKey(event, capitalGainDK));
-shareAmountInput.addEventListener("keydown", (event) => handleEnterKey(event, capitalGainDK));
+salesPriceInput.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.keyCode === 13) {
+        event.preventDefault();
+        reset();
+    } else if (event.keyCode === 13) {
+        event.preventDefault();
+        if (validateInputsAfkastBtn()) {
+            capitalGain();
+        }
+    }
+});
+
+shareAmountInput.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.keyCode === 13) {
+        event.preventDefault();
+        reset();
+    } else if (event.keyCode === 13) {
+        event.preventDefault();
+        if (validateInputsAfkastBtn()) {
+            capitalGain();
+        }
+    }
+});
+
 
 // Functions
 function validateInputsAfkastBtn() {
@@ -101,7 +103,13 @@ function validateInputsAfkastBtn() {
     let salesPrice = parseFloat(salesPriceInput.value);
     let shareAmount = parseFloat(shareAmountInput.value);
 
-    if (isNaN(currentPrice) || isNaN(salesPrice) || isNaN(shareAmount) || currentPrice < 0 || salesPrice < 0 || shareAmount < 0) {
+    if (isNaN(currentPrice) || isNaN(salesPrice) || isNaN(shareAmount)) {
+        returnValue.textContent = missingInputWarning;
+        returnValue.style.color = red;
+        return false;
+    }
+
+    if (currentPrice < 0 || salesPrice < 0 || shareAmount < 0) {
         returnValue.textContent = missingInputWarning;
         returnValue.style.color = red;
         return false;
@@ -114,8 +122,15 @@ function validateInputsAfkastBtn() {
 function validateInputsProcentBtn() {
     let currentPrice = parseFloat(currentPriceInput.value);
     let salesPrice = parseFloat(salesPriceInput.value);
+    let shareAmount = parseFloat(shareAmountInput.value);
 
-    if (isNaN(currentPrice) || isNaN(salesPrice) || currentPrice < 0 || salesPrice < 0) {
+    if (isNaN(currentPrice) || isNaN(salesPrice)) {
+        returnValue.textContent = missingInputWarning;
+        returnValue.style.color = red;
+        return false;
+    }
+
+    if (currentPrice < 0 || salesPrice < 0 || shareAmount < 0) {
         returnValue.textContent = missingInputWarning;
         returnValue.style.color = red;
         return false;
@@ -125,100 +140,38 @@ function validateInputsProcentBtn() {
     return true;
 }
 
-function capitalGainDK() {
-    let shareAmount = parseFloat(shareAmountInput.value);
+function capitalGain() {
+    let currentPrice = currentPriceInput.value;
+    let salesPrice = salesPriceInput.value;
+    let shareAmount = shareAmountInput.value;
 
-    if (!validateInputsAfkastBtn()) {
-        return;
-    }
-
-    let profit = calculateProfitAfterCosts(shareAmount);
-    let formattedNumber = formatNumber(profit);
-
-    returnValue.textContent = formattedNumber + " DKK";
-    colorDecider(profit);
-}
-
-function capitalGainUSD() {
-    let shareAmount = parseFloat(shareAmountInput.value);
-
-    if (!validateInputsAfkastBtn()) {
-        return;
-    }
-
-    let profit = calculateProfitAfterCosts(shareAmount);
-    let formattedNumber = formatNumber(profit);
-
-    returnValue.textContent = formattedNumber + " USD";
-    colorDecider(profit);
-}
-
-function capitalGainEURO() {
-    let currentPrice = parseFloat(currentPriceInput.value);
-    let salesPrice = parseFloat(salesPriceInput.value);
-    let shareAmount = parseFloat(shareAmountInput.value);
-
-    if (!validateInputsAfkastBtn()) {
-        return;
-    }
-
-    let profit = calculateProfitAfterCosts(shareAmount);
-    let formattedNumber = formatNumber(profit);
-
-    returnValue.textContent = formattedNumber + " EURO";
-    colorDecider(profit);
-}
-
-function validateInputs(currentPrice, salesPrice, shareAmount) {
     if (isNaN(currentPrice) || isNaN(salesPrice) || isNaN(shareAmount)) {
         returnValue.textContent = invalidInputWarning;
         returnValue.style.color = red;
-        return false;
+        return;
     }
-    return true;
-}
 
-function calculateProfitAfterCosts(shareAmount) {
     let profitPerShare = shareDifference();
-    let profitBeforeCosts = shareAmount * profitPerShare;
+    let profit = shareAmount * profitPerShare;
 
-    return profitAfterBrokerageAndTax(profitBeforeCosts);
-}
+    let brokerage = 20; // 20 kr. i kurtage
+    let tax = 1 - 0.17;
+
+    profit -= brokerage;
+    profit *= tax;
+
+    let formattedNumber = formatNumber(profit);
+
+    returnValue.textContent = formattedNumber + " kr";
+    colorDecider(profit);
+};
 
 function shareDifference() {
-    let currentPrice = parseFloat(currentPriceInput.value);
-    let salesPrice = parseFloat(salesPriceInput.value);
+    let currentPrice = currentPriceInput.value;
+    let salesPrice = salesPriceInput.value;
 
     return salesPrice - currentPrice;
-}
-
-function profitAfterBrokerageAndTax(profitBeforeCost) {
-    let brokerage = 20; // 20 kr. i kurtage (2x 10 kr.)
-    let tax = 1 - 0.17;
-    let exchangeFee = calculateExchangeFee();
-
-    profitBeforeCost -= brokerage;
-    profitBeforeCost -= exchangeFee;
-    profitBeforeCost *= tax;
-
-    return profitBeforeCost;
-}
-
-function calculateExchangeFee() {
-    let currency = currencyDropdown.value.toLowerCase();
-    let exchangeFee = 0;
-
-    if (currency === "dkk") {
-        exchangeFee = 0;
-    } else if (currency === "usd") {
-        let usdAmount = parseFloat(exchangeFeeInput.value);
-        exchangeFee = usdAmount * USD;
-    } else if (currency === "euro") {
-        let euroAmount = parseFloat(exchangeFeeInput.value);
-        exchangeFee = euroAmount * EURO;
-    }
-    return exchangeFee;
-}
+};
 
 function formatNumber(number) {
     if (isNaN(number)) {
@@ -234,7 +187,7 @@ function formatNumber(number) {
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
         return parts.join(",");
     }
-}
+};
 
 function colorDecider(value) {
     if (value > 0) {
@@ -243,11 +196,12 @@ function colorDecider(value) {
         returnValue.style.color = red;
     } else {
         returnValue.style.color = grey;
-    }
-}
+    };
+};
+
 
 function calculateProcentage() {
-    let currentPrice = parseFloat(currentPriceInput.value);
+    let currentPrice = currentPriceInput.value;
     let calculatedProcentage = 0;
 
     let profitPerShare = shareDifference();
@@ -256,29 +210,13 @@ function calculateProcentage() {
     colorDecider(calculatedProcentage);
 
     returnValue.textContent = calculatedProcentage.toFixed(1) + "%";
-}
+};
 
 function reset() {
     returnValue.textContent = 0;
     currentPriceInput.value = "";
     salesPriceInput.value = "";
     shareAmountInput.value = "";
-    exchangeFeeInput.value = "";
 
     returnValue.style.color = grey;
-}
-
-function toggleExchangeFee() {
-    const vekselgebyrGroup = document.getElementById("vekselgebyrGroup");
-    const selectedCurrency = currencyDropdown.value.toLowerCase();
-
-    if (selectedCurrency === "dkk") {
-        vekselgebyrGroup.style.display = "none";
-    } else {
-        vekselgebyrGroup.style.display = "block";
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    toggleExchangeFee();
-});
+};
